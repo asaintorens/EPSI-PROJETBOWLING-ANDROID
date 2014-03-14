ab2b99d6d1cd4f45cd83c98379629f5001c91592
Alexandre Barbez <alexandre.barbez@gmail.com>
2014-03-14 15:14:27 +0100

    Création projet

---
+++ src/com/example/ProjetBowlingAndroid/MonBowling.java
@@ -0,0 +1,15 @@
+package com.example.ProjetBowlingAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MonBowling extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
